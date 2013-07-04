8313f38190f019af77a6f078321027c40afc4418
masonwheeler <masonwheeler@2c06af34-a49c-fd6d-87c6-ea1b3c63e27d>
2013-07-04 23:15:20 +0000

    Fixing an error when using multiple units in the same program.

--- trunk/source/rsExec.pas
+++ trunk/source/rsExec.pas
@@ -321,13 +321,24 @@ begin
 end;
 
 procedure TrsExec.CreateMethodPointer(method: TRttiMethod; var address: pointer);
+const RSUNIT_HEADER = 'rsUNIT*.';
 var
    impl: TMethodImplementation;
+   sl: TStringList;
+   unitname: string;
+   idx: integer;
 begin
    impl := method.CreateImplementation(address, self.MethodImplementation);
-   address := impl.CodeAddress;
+   sl := FProgram.unitOffsets;
+   unitname := method.parent.name;
+   AnsiStartsText(RSUNIT_HEADER, unitName);
+   unitName := Copy(unitName, length(RSUNIT_HEADER));
+   idx := sl.IndexOf(unitName);
+   assert(idx >= 0);
+   nativeUint(address) := nativeUInt(impl.CodeAddress) + nativeUInt(pointer(sl.Objects[idx]));
    FImpls.Add(impl);
-   FProcMap.Add(method.Name, method)
+   FProcMap.Add(method.Name, method);
+   sl.Text;
 end;
 
 function TrsExec.DoImport(const unitName: string): TDictionary<string, pointer>;
